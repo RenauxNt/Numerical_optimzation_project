9393519b4c746c34e4fddbf9a03d19af8307ca8e
RenauxNt <127757149+RenauxNt@users.noreply.github.com>
2024-11-14 19:22:35 +0100

    Two first questions done

--- file.jl
+++ file.jl
@@ -15,16 +15,30 @@ capital_intial = 500000
 
 # Variables
 @variable(model, 0.2 >= parts[1:num_stocks] >= 0)
-@variable(model, returns[1:num_stocks, new_weeks])
+#@variable(model, returns[1:num_stocks, new_weeks])
 
 # Constraints
-@constraint(model, 0 <= sum(parts[stock] for stock in 1:num_stocks) <= 1)
+@constraint(model, 0 <= sum(parts[i] for i in 1:num_stocks) <= 1)
 
 for num_sector in num_sectors
     sector_i = findall(sectors[:,2] .== num_sector)
-    @constraints(sum(parts[stock] for stock in sector_i) <= 0.2)
+    @constraint(model, sum(parts[i] for i in sector_i) <= 0.2)
 end
 
 # Objective: maximize historical average weekly return.
 # We do not put the normalization factor in the objective function because it does not affect the optimal solution.
-@objective(model, Max, sum(returns[stock, week] * parts[stock] for stock in 1:num_stocks, week in 1:num_weeks))
+@objective(model, Max, sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) * parts[i] for i in 1:num_stocks, week in 2:num_weeks))
+
+# Solve the model
+optimize!(model)
+
+if termination_status(model) == MOI.OPTIMAL
+    optimal_allocation = value.(parts)
+    index = findall(optimal_allocation[:] .!= 0)
+    total_return = objective_value(model)
+    println("Optimal Portfolio Allocation:")
+    println(names(stocks[:,index]), " with ", optimal_allocation[index], (100000/stocks[1, i] for i in index))
+    println("Total Historical Average Weekly Return:", total_return)
+else
+    println("No optimal solution found")
+end
