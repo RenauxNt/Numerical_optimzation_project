00b11df0e558160c505409ed6704e196b6fb3a29
RenauxNt <127757149+RenauxNt@users.noreply.github.com>
2024-11-20 09:14:27 +0100

    Questions 3 and 4

--- file.jl
+++ file.jl
@@ -14,15 +14,16 @@ num_sectors = unique(sectors[:,2])
 capital_intial = 500000
 
 # Variables
-@variable(model, 0.2 >= parts[1:num_stocks] >= 0)
+c1 = @variable(model, parts[1:num_stocks] >= 0)
 #@variable(model, returns[1:num_stocks, new_weeks])
 
 # Constraints
-@constraint(model, 0 <= sum(parts[i] for i in 1:num_stocks) <= 1)
+full_capital_constraint= @constraint(model, 0 <= sum(parts[i] for i in 1:num_stocks) <= 1)
 
+sector_constraints = Dict()
 for num_sector in num_sectors
     sector_i = findall(sectors[:,2] .== num_sector)
-    @constraint(model, sum(parts[i] for i in sector_i) <= 0.2)
+    sector_constraints[num_sector] =@constraint(model, sum(parts[i] for i in sector_i) <= 0.2)
 end
 
 # Objective: maximize historical average weekly return.
@@ -34,11 +35,23 @@ optimize!(model)
 
 if termination_status(model) == MOI.OPTIMAL
     optimal_allocation = value.(parts)
-    index = findall(optimal_allocation[:] .!= 0)
+    index_parts = findall(optimal_allocation[:] .!= 0)
     total_return = objective_value(model)
     println("Optimal Portfolio Allocation:")
-    println(names(stocks[:,index]), " with ", optimal_allocation[index], (100000/stocks[1, i] for i in index))
-    println("Total Historical Average Weekly Return:", total_return)
+    for i in index_parts
+        println(names(stocks)[i]," from secotr ", sectors[i,2] ," with ", (100000/stocks[1, i]), " shares and ", 
+        sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks)/num_weeks*100, "% of weekly return")
+    end
+    println("Total Historical Average Weekly Return:",  sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i])*100 for week in 2:num_weeks, i in index_parts)/num_weeks,"%")
+    println(optimal_allocation[index_parts])
+    println("\n DUAL VARIABLES \n")
+    println("p: ", dual(full_capital_constraint))
+
+    # Dual values for sector allocation constraints
+    for (sector, constraint) in sector_constraints
+        println("q_", sector, " ", dual(constraint))
+    end
+
 else
     println("No optimal solution found")
 end
