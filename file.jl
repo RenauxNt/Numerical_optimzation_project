b615cf64c46db8d668e93d369b9e003eb4152b78
RenauxNt <127757149+RenauxNt@users.noreply.github.com>
2024-11-20 18:40:30 +0100

    GOOD PROBLEM

--- file.jl
+++ file.jl
@@ -28,7 +28,7 @@ end
 
 # Objective: maximize historical average weekly return.
 # We do not put the normalization factor in the objective function because it does not affect the optimal solution.
-@objective(model, Max, sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) * parts[i] for i in 1:num_stocks, week in 2:num_weeks))
+@objective(model, Max, sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i])*parts[i] for i in 1:num_stocks, week in 2:num_weeks)/(num_weeks-1)*100)
 
 # Solve the model
 optimize!(model)
@@ -40,18 +40,12 @@ if termination_status(model) == MOI.OPTIMAL
     println("Optimal Portfolio Allocation:")
     for i in index_parts
         println(names(stocks)[i]," from secotr ", sectors[i,2] ," with ", (100000/stocks[1, i]), " shares and ", 
-        sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks)/num_weeks*100, "% of weekly return")
+                optimal_allocation[i]*100 ,"% of the capital invest and ", 
+                sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks)/(num_weeks-1)*100, "% of weekly return")
     end
-    println("Total Historical Average Weekly Return:",  sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i])*100 for week in 2:num_weeks, i in index_parts)/num_weeks,"%")
+    println("Total Historical Average Weekly Return:",  sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks, i in index_parts)/(num_weeks-1)*100,"%")
+    println(total_return*5)
     println(optimal_allocation[index_parts])
-    println("\n DUAL VARIABLES \n")
-    println("p: ", dual(full_capital_constraint))
-
-    # Dual values for sector allocation constraints
-    for (sector, constraint) in sector_constraints
-        println("q_", sector, " ", dual(constraint))
-    end
-
 else
     println("No optimal solution found")
 end
