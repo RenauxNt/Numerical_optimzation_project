29652b7e0b9f6e8cc58a7363c33c1095f6bedfcd
robzz112 <robert.florea@student.uliege.be>
2024-11-21 12:39:35 +0100

    updates

--- Q8.jl
+++ Q8.jl
@@ -1,4 +1,4 @@
-using JuMP, Gurobi, CSV, DataFrames
+using JuMP, Gurobi, CSV, DataFrames, Statistics
 
 # Create a new JuMP model with Gurobi as the solver
 
@@ -15,11 +15,11 @@ num_sectors = unique(sectors[:,2])
 capital_intial = 500000
 
 # Covariance matrix
-sigma = cov(Matrix(stocks[:,1:end]))
+sigma = cov( Matrix(stocks[:,2:end]) )
 
-for gamma in gammas
+for gamma in Array(gammas)
     model = Model(Gurobi.Optimizer)
-    set_optimizer_attribute(model, "Method", 0)  # Primal Simplex
+    #set_optimizer_attribute(model, "Method", 0) # Primal Simplex
     #set_optimizer_attribute(model, "Method", 1) # Dual Simplex
 
     # Variables
@@ -31,12 +31,14 @@ for gamma in gammas
     sector_constraints = Dict()
     for num_sector in num_sectors
         sector_i = findall(sectors[:,2] .== num_sector)
-        sector_constraints[num_sector] =@constraint(model, sum(parts[i] for i in sector_i) <= 0.2)
+        sector_constraints[num_sector] = @constraint(model, sum(parts[i] for i in sector_i) <= 0.2)
     end
 
     # Objective: maximize historical average weekly return.
     # We do not put the normalization factor in the objective function because it does not affect the optimal solution.
-    @objective(model, Max, sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) * parts[i] for i in 1:num_stocks, week in 2:num_weeks)*100/(num_weeks-1))
+    local expected_return = sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) * parts[i] for i in 1:num_stocks, week in 2:num_weeks)*100/(num_weeks-1)
+    risk = sum(parts[i]*sigma[i,j]*parts[j] for i in 1:(num_stocks-1), j in 1:(num_stocks-1))
+    @objective(model, Max, expected_return - gamma * risk)
 
     # Solve the model
     optimize!(model)
@@ -45,9 +47,9 @@ for gamma in gammas
         optimal_allocation = value.(parts)
         index_parts = findall(optimal_allocation[:] .!= 0)
         total_return = objective_value(model)
-        println("Optimal Portfolio Allocation:")
+        println("Optimal Portfolio Allocation for gamma = ", gamma," :")
         for i in index_parts
-            println(names(stocks)[i]," from secotr ", sectors[i,2] ," with ", (100000/stocks[1, i]), " shares and ", 
+            println(names(stocks)[i]," from sector ", sectors[i,2] ," with ", (100000/stocks[1, i]), " shares and ", 
             sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks)*100/(num_weeks-1), "% of weekly return")
         end
         println("Total Historical Average Weekly Return:",  sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks, i in index_parts)*100/(num_weeks-1),"%")
