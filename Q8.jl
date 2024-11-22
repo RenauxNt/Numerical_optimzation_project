8210a56a2d1db3165fa720e7926ecc653feffbf6
FloreaRob <robert.florea@student.uliege.be>
2024-11-22 11:38:14 +0100

    Q8 updates

--- Q8.jl
+++ Q8.jl
@@ -1,7 +1,6 @@
-using JuMP, Gurobi, CSV, DataFrames, Statistics
+using JuMP, Gurobi, CSV, DataFrames, Statistics, Plots
 
 # Create a new JuMP model with Gurobi as the solver
-
 
 #Loadings the CSV files
 stocks = CSV.read("data.csv", DataFrame)
@@ -15,29 +14,35 @@ num_sectors = unique(sectors[:,2])
 capital_intial = 500000
 
 # Covariance matrix
-sigma = cov( Matrix(stocks[:,2:end]) )
+sigma = cov( Matrix(stocks[:,1:end]) )
+
+model = Model(Gurobi.Optimizer)
+set_optimizer_attribute(model, "Method", 0) # Primal Simplex
+#set_optimizer_attribute(model, "Method", 1) # Dual Simplex
+
+# Variables
+@variable(model, parts[1:num_stocks] >= 0)
+
+# Constraints
+full_capital_constraint= @constraint(model, 0 <= sum(parts[i] for i in 1:num_stocks) <= 1)
+
+sector_constraints = Dict()
+for num_sector in num_sectors
+    sector_i = findall(sectors[:,2] .== num_sector)
+    sector_constraints[num_sector] = @constraint(model, sum(parts[i] for i in sector_i) <= 0.2)
+end
+
+expected_return = sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) * parts[i] for i in 1:num_stocks, week in 2:num_weeks)*100/(num_weeks-1)
+risk = sum(transpose(parts)*sigma*parts)
+#risk = sum(parts[i] * sigma[i, j] * parts[j] for i in 1:num_stocks, j in 1:num_stocks)
+
+risks = []
+expected_returns = []
 
 for gamma in Array(gammas)
-    model = Model(Gurobi.Optimizer)
-    #set_optimizer_attribute(model, "Method", 0) # Primal Simplex
-    #set_optimizer_attribute(model, "Method", 1) # Dual Simplex
-
-    # Variables
-    @variable(model, parts[1:num_stocks] >= 0)
-
-    # Constraints
-    full_capital_constraint= @constraint(model, 0 <= sum(parts[i] for i in 1:num_stocks) <= 1)
-
-    sector_constraints = Dict()
-    for num_sector in num_sectors
-        sector_i = findall(sectors[:,2] .== num_sector)
-        sector_constraints[num_sector] = @constraint(model, sum(parts[i] for i in sector_i) <= 0.2)
-    end
-
+    println("Gamma: ", gamma)
     # Objective: maximize historical average weekly return.
     # We do not put the normalization factor in the objective function because it does not affect the optimal solution.
-    local expected_return = sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) * parts[i] for i in 1:num_stocks, week in 2:num_weeks)*100/(num_weeks-1)
-    risk = sum(parts[i]*sigma[i,j]*parts[j] for i in 1:(num_stocks-1), j in 1:(num_stocks-1))
     @objective(model, Max, expected_return - gamma * risk)
 
     # Solve the model
@@ -48,20 +53,35 @@ for gamma in Array(gammas)
         index_parts = findall(optimal_allocation[:] .!= 0)
         total_return = objective_value(model)
         println("Optimal Portfolio Allocation for gamma = ", gamma," :")
-        for i in index_parts
-            println(names(stocks)[i]," from sector ", sectors[i,2] ," with ", (100000/stocks[1, i]), " shares and ", 
-            sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks)*100/(num_weeks-1), "% of weekly return")
-        end
-        println("Total Historical Average Weekly Return:",  sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks, i in index_parts)*100/(num_weeks-1),"%")
-        println(optimal_allocation[index_parts])
-        println("\n DUAL VARIABLES \n")
-        println("p:  ", dual(full_capital_constraint))
+
+        println("Total Historical Average Weekly Return:",  sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i])*optimal_allocation[i] for week in 2:num_weeks, i in index_parts)*100/(num_weeks-1),"%")
+
+        println("Risk: ", value.(risk))
+        println("Expected Return: ", value.(expected_return))
+
+        push!(risks, value.(risk))
+        push!(expected_returns, value.(expected_return - gamma * risk))
+
+        # optimal_allocation = value.(parts)
+        # index_parts = findall(optimal_allocation[:] .!= 0)
+        # total_return = objective_value(model)
+        # println("Optimal Portfolio Allocation for gamma = ", gamma," :")
+        # for i in index_parts
+        #     println(names(stocks)[i]," from sector ", sectors[i,2] ," with ", (100000/stocks[1, i]), " shares and ", 
+        #     sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks)*100/(num_weeks-1), "% of weekly return")
+        # end
+        # println("Total Historical Average Weekly Return:",  sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks, i in index_parts)*100/(num_weeks-1),"%")
+        # println(optimal_allocation[index_parts])
+        # println("\n DUAL VARIABLES \n")
+        # println("p:  ", dual(full_capital_constraint))
 
         # Dual values for sector allocation constraints
-        for (sector, constraint) in sector_constraints
-            println("q_", sector, " ", dual(constraint))
-        end
+        # for (sector, constraint) in sector_constraints
+        #     println("q_", sector, " ", dual(constraint))
+        # end
     else
         println("No optimal solution found")
     end
 end
+
+Plots.plot(risks, expected_returns)
