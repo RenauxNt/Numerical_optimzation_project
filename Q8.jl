f2e2acbed1bb8abe16080135e7b71a82193d8998
FloreaRob <robert.florea@student.uliege.be>
2024-11-23 15:10:52 +0100

    Last push on my branch

--- Q8.jl
+++ Q8.jl
@@ -12,19 +12,18 @@ num_weeks = nrow(stocks)
 num_stocks = nrow(sectors)
 num_sectors = unique(sectors[:,2])
 capital_intial = 500000
-
-# Covariance matrix
-sigma = cov( Matrix(stocks[:,1:end]) )
+const_obj = 100/(num_weeks-1)
 
 model = Model(Gurobi.Optimizer)
-set_optimizer_attribute(model, "Method", 0) # Primal Simplex
-#set_optimizer_attribute(model, "Method", 1) # Dual Simplex
 
 # Variables
 @variable(model, parts[1:num_stocks] >= 0)
 
+# Covariance matrix
+sigma = cov([(stocks[week, i] - stocks[week-1, i]) / stocks[week-1, i] * 100 for week in 2:num_weeks, i in 1:num_stocks])
+
 # Constraints
-full_capital_constraint= @constraint(model, 0 <= sum(parts[i] for i in 1:num_stocks) <= 1)
+full_capital_constraint= @constraint(model, sum(parts[i] for i in 1:num_stocks) <= 1)
 
 sector_constraints = Dict()
 for num_sector in num_sectors
@@ -32,15 +31,13 @@ for num_sector in num_sectors
     sector_constraints[num_sector] = @constraint(model, sum(parts[i] for i in sector_i) <= 0.2)
 end
 
-expected_return = sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) * parts[i] for i in 1:num_stocks, week in 2:num_weeks)*100/(num_weeks-1)
-risk = sum(transpose(parts)*sigma*parts)
-#risk = sum(parts[i] * sigma[i, j] * parts[j] for i in 1:num_stocks, j in 1:num_stocks)
+expected_return = sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) * parts[i] for i in 1:num_stocks, week in 2:num_weeks)*const_obj
+risk = sum(transpose(parts) * sigma * parts)
 
 risks = []
 expected_returns = []
 
 for gamma in Array(gammas)
-    println("Gamma: ", gamma)
     # Objective: maximize historical average weekly return.
     # We do not put the normalization factor in the objective function because it does not affect the optimal solution.
     @objective(model, Max, expected_return - gamma * risk)
@@ -50,35 +47,14 @@ for gamma in Array(gammas)
 
     if termination_status(model) == MOI.OPTIMAL
         optimal_allocation = value.(parts)
-        index_parts = findall(optimal_allocation[:] .!= 0)
-        total_return = objective_value(model)
-        println("Optimal Portfolio Allocation for gamma = ", gamma," :")
+        index_parts = findall(optimal_allocation .!= 0)
 
-        println("Total Historical Average Weekly Return:",  sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i])*optimal_allocation[i] for week in 2:num_weeks, i in index_parts)*100/(num_weeks-1),"%")
-
+        return_val = sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i])*optimal_allocation[i] for week in 2:num_weeks, i in index_parts)*const_obj
+        println("Total Historical Average Weekly Return:",  return_val, "% for gamma = ", gamma)
         println("Risk: ", value.(risk))
-        println("Expected Return: ", value.(expected_return))
-
+        
         push!(risks, value.(risk))
-        push!(expected_returns, value.(expected_return - gamma * risk))
-
-        # optimal_allocation = value.(parts)
-        # index_parts = findall(optimal_allocation[:] .!= 0)
-        # total_return = objective_value(model)
-        # println("Optimal Portfolio Allocation for gamma = ", gamma," :")
-        # for i in index_parts
-        #     println(names(stocks)[i]," from sector ", sectors[i,2] ," with ", (100000/stocks[1, i]), " shares and ", 
-        #     sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks)*100/(num_weeks-1), "% of weekly return")
-        # end
-        # println("Total Historical Average Weekly Return:",  sum(((stocks[week, i]-stocks[week-1, i])/stocks[week-1, i]) for week in 2:num_weeks, i in index_parts)*100/(num_weeks-1),"%")
-        # println(optimal_allocation[index_parts])
-        # println("\n DUAL VARIABLES \n")
-        # println("p:  ", dual(full_capital_constraint))
-
-        # Dual values for sector allocation constraints
-        # for (sector, constraint) in sector_constraints
-        #     println("q_", sector, " ", dual(constraint))
-        # end
+        push!(expected_returns, value.(return_val))
     else
         println("No optimal solution found")
     end
@@ -89,6 +65,4 @@ Plots.plot(risks, expected_returns, label="Efficient Frontier", lw=2, size = (80
 Plots.title!("Evolution of the expected return as a function of the risk")
 Plots.xlabel!("Risk")
 Plots.ylabel!("Expected Return")
-Plots.xlims!(0, 40)
-Plots.ylims!(0, 0.4)
 Plots.savefig("efficient_frontier.pdf")
